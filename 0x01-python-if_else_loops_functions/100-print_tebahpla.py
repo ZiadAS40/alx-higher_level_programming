d16a20bb4f5243f75bbdc913b1bb474f463758b9
ZiadAS40 <zead4036@gmail.com>
2024-03-12 21:54:59 +0200

    add the last three tasks

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 for n in range(ord('z'), ord('a') - 1, -1):
-    if n % 2 != 0:
-        print("{:c}".format(n - 32), end='')
-    else:
-        print("{:c}".format(n), end='')
+    print("{}".format(chr(n - i)), end="")
+    i = 32 if i == 0 else 0
