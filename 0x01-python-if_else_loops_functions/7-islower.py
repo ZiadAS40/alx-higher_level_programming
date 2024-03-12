4f6cceb569928da9f04453962e3785868451b9f6
ZiadAS40 <zead4036@gmail.com>
2024-03-12 17:34:20 +0200

    fix pycodestyle

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def islower(c):
-	cpy = ord(c)
-	if cpy < 97:
-		return False
-	else:
-		return True
+    cpy = ord(c)
+    if cpy < 97:
+        return False
+    else:
+        return True
