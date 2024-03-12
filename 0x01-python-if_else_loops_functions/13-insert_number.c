6d36d13afb682021883fcac7a05205d5147f5245
ZiadAS40 <zead4036@gmail.com>
2024-03-12 21:29:25 +0200

    fix some issues

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -6,27 +6,33 @@
  * @number: the number that should be iserted.
  * Return: pointer to the first node if success
  * NULL if fails
-*/
+ */
 
 listint_t *insert_node(listint_t **head, int number)
 {
 	listint_t *temp = *head;
 	listint_t *curr = *head;
 
+	if (number <= (*head)->n)
+	{
+		add_nodeint(head, number);
+		return (*head);
+	}
 	while (temp != NULL)
 	{
 		temp = temp->next;
 		if (number >= curr->n && number < temp->n)
 		{
-           temp = malloc(sizeof(listint_t));
-		   if (!temp)
-		   return (NULL);
-		   temp->n = number;
-		   temp->next = curr->next;
-		   curr->next = temp;
-		   return (*head);
+			temp = malloc(sizeof(listint_t));
+			if (!temp)
+				return (NULL);
+			temp->n = number;
+			temp->next = curr->next;
+			curr->next = temp;
+			return (*head);
 		}
 		curr = curr->next;
 	}
-		return (NULL);
+	add_nodeint_end(head, number);
+	return (*head);
 }
