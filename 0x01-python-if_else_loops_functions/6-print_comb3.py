5ef48ec44fd5f5bf442845b61805b456bcf9c5d8
ZiadAS40 <zead4036@gmail.com>
2024-03-12 17:14:47 +0200

    fix some issues

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,7 +2,8 @@
 list = list(range(0, 10))
 for i in range(0, 10):
     for j in list:
-        if (i != j) and (i != 8) and (j != 9):
-            print("{}{}, ".format(i, j), end='')
+        if (i != j):
+            if (i != 9) and (i != 8):
+                print("{}{}, ".format(i, j), end='')
     list.pop(0)
 print("89")
