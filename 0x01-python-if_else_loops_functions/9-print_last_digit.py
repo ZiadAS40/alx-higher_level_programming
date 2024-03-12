ea9e356b06389549b881e5afe988d39e3bc3aa11
ZiadAS40 <zead4036@gmail.com>
2024-03-12 18:50:32 +0200

    fix some issues

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_last_digit(number):
     if (number < 0):
-        ncpy = number * -1
-        return (ncpy % 10) * -1
-    else:
-        return number % 10
+        number = number * -1
+        return number % 10 * -1
+    return number % 10
+	
