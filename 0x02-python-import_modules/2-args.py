f3bf4e59563db9197eb4e1e4aea376deb770ec5c
ZiadAS40 <zead4036@gmail.com>
2024-03-14 20:41:21 +0200

    fix some issues

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,6 +3,6 @@ import sys
 arg = sys.argv
 if __name__ == "__main__":
     nls = len(arg)
-    print("{} arguments{}".format(nls - 1, ':' if nls > 1 else '.'))
+    print("{} {}{}".format(nls - 1, "argument" if nls - 1 == 1 else "arguments", ':' if nls > 1 else '.'))
     for n in range(1, nls):
         print("{}: {}".format(n, arg[n]))
