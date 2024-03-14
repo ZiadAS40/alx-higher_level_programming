bf222644021ce50dc10db12e33ec5c9d5c1e0970
ZiadAS40 <zead4036@gmail.com>
2024-03-14 17:32:41 +0200

    fix some issues

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -10,13 +10,13 @@ if __name__ == "__main__":
         a = int(arg[1])
         b = int(arg[3])
         if arg[2] == '+':
-            print(add(a, b))
+            print("{} + {} = {}".format(a, b, add(a, b)))
         elif arg[2] == '-':
-            print(sub(a, b))
+            print("{} - {} = {}".format(a, b, sub(a, b)))
         elif arg[2] == '*':
-            print(mul(a, b))
+            print("{} * {} = {}".format(a, b, mul(a, b)))
         elif arg[2] == '/':
-            print(div(a, b))
+            print("{} / {} = {}".format(a, b, div(a, b)))
         else:
             print("Unknown operator. Available operators: +, -, * and /")
             sys.exit(1)
