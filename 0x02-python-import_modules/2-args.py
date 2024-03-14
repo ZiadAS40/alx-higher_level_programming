f2012c8e4ccbb89270e224001ab9d110540d08f0
ZiadAS40 <zead4036@gmail.com>
2024-03-14 20:45:28 +0200

    fix some issues

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,6 +3,8 @@ import sys
 arg = sys.argv
 if __name__ == "__main__":
     nls = len(arg)
-    print("{} {}{}".format(nls - 1, "argument" if nls - 1 == 1 else "arguments", ':' if nls > 1 else '.'))
+    print("{} ".format(nls - 1), end='')
+    print("{}".format("argument" if nls - 1 == 1 else "arguments"), end='')
+    print("{}".format(':' if nls > 1 else '.'))
     for n in range(1, nls):
         print("{}: {}".format(n, arg[n]))
