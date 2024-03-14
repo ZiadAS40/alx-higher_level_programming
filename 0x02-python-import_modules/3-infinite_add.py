081a0815fc9ee6d6729e87ab1fa8eb00c23ff415
ZiadAS40 <zead4036@gmail.com>
2024-03-14 16:35:22 +0200

    fix some issues

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 import sys
-arg = sys.argv
-result = 0
-for n in range(1, len(arg)):
-    result += int(arg[n])
-print("{}".format(result))
+if __name__ == "__main__":
+    arg = sys.argv
+    result = 0
+    for n in range(1, len(arg)):
+        result += int(arg[n])
+    print("{}".format(result))
