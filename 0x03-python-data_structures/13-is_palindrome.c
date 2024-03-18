a0b79349afee8dfa9b60f9791046f11a2ed9018f
ZiadAS40 <zead4036@gmail.com>
2024-03-18 13:10:42 +0200

    fix some issues

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -9,39 +9,22 @@
 int is_palindrome(listint_t **head)
 {
 	listint_t *tmp = *head;
-	int i = 0, buffer_size = 128;
-	int *buffer = malloc(sizeof(int) * buffer_size), *tmp_buffer;
+	int i = 0;
+	int buffer[1024];
 
-	if (!buffer)
-		return (-1);
 	while (tmp != NULL)
 	{
 		buffer[i] = tmp->n;
 		tmp = tmp->next;
 		i++;
-		if (i >= buffer_size)
-		{
-			buffer_size += 128;
-			tmp_buffer = realloc(buffer, sizeof(int) * buffer_size);
-			if (!tmp_buffer)
-			{
-				free(buffer);
-				return (-1);
-			}
-			buffer = tmp_buffer;
-		}
 	}
 	i--;
 	while ((*head) != NULL)
 	{
 		if ((*head)->n != buffer[i])
-		{
-			free(buffer);
 			return (0);
-		}
 		(*head) = (*head)->next;
 		i--;
 	}
-	free(buffer);
 	return (1);
 }
