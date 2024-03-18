d97dbc135637118135967b293c2cf2ac3c8ed5da
ZiadAS40 <zead4036@gmail.com>
2024-03-18 10:12:55 +0200

    fix some issues

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,7 +3,9 @@ def max_integer(my_list=[]):
     buffer = 0
     if len(my_list) == 0:
         return None
-    for i in range(0 ,len(my_list)):
+    elif len(my_list) == 1:
+        return my_list[0]
+    for i in range(0, len(my_list)):
         if i + 1 == len(my_list):
             break
         if my_list[i] > my_list[i + 1] and my_list[i] > buffer:
