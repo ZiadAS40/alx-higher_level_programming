4787756ccafff66ed68384040b529f872fd2ace2
ZiadAS40 <zead4036@gmail.com>
2024-03-22 16:44:21 +0200

    fix some issues

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,12 +2,18 @@
 def roman_to_int(roman_string):
     rA = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     result = 0
+    skip_next = False
     for i in range(0, len(roman_string)):
+        if skip_next:
+            continue
         if i != len(roman_string) - 1:
             if rA[roman_string[i]] < rA[roman_string[i + 1]]:
                 result += rA[roman_string[i + 1]] - rA[roman_string[i]]
+                skip_next = True
             else:
                 result += rA[roman_string[i]]
+                skip_next = False
         else:
             result += rA[roman_string[i]]
+            skip_next = False
     return result
