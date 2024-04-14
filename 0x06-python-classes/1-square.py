48e9f858b4c157f0272b9b19c9e5c47d22a5dd82
ZiadAS40 <zead4036@gmail.com>
2024-04-14 15:10:32 +0200

    adding the second task

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""defines a class depends on the previuos one"""
+
+
+class Square:
+    """with just only an instance private attribute"""
+    def __init__(self, size):
+        self.__size = size
