d87998939af98ef6f35345882cfc0c7b509fcd59
ZiadAS40 <zead4036@gmail.com>
2024-04-15 23:37:01 +0200

    add the decomentation to all the files

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -3,6 +3,10 @@
 
 
 class MyList(list):
+    """a subclass of list"""
+    def __init__(self):
+        """initializes the object"""
+        super().__init__()
     """defines a func to print this list sorted"""
     def print_sorted(self):
         print(sorted(self))
--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -4,4 +4,4 @@
 
 def is_same_class(obj, a_class):
     """perform it"""
-    return (type(obj) == a_class)
+    return type(obj) == a_class
