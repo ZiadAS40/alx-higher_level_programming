9d1da3891b83425a34971313b9f6cf662c37f5a1
ZiadAS40 <zead4036@gmail.com>
2024-04-15 23:29:18 +0200

    adding the decomentation

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+"""checks if it is only a subclass"""
+
+
+
 def inherits_from(obj, a_class):
+    """perform the checks"""
     if isinstance(obj, a_class):
         return type(obj) != a_class
     return False
