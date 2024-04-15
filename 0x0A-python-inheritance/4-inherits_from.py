32fc84cc1fc36bb2736d2a3c304fb07f2b5170c6
ZiadAS40 <zead4036@gmail.com>
2024-04-15 23:27:38 +0200

    fix a small  mistake

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def is_instance_of_subclass_only(obj, a_class):
+def inherits_from(obj, a_class):
     if isinstance(obj, a_class):
         return type(obj) != a_class
     return False
