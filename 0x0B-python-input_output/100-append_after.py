ea340a7b45ae70bdb09db0f6d1e3d57f2dad42bb
ZiadAS40 <zead4036@gmail.com>
2024-04-18 00:47:03 +0200

    fix pycodestyle;

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -6,7 +6,8 @@ isert a line after a certain string
 
 def append_after(filename="", search_string="", new_string=""):
     """
-    a function that inserts a line of text to a file, after each line containing a specific string 
+    a function that inserts a line of text
+    to a file, after each line containing a specific string
     """
     with open(filename, 'r', encoding='utf-8') as f:
         myList = []
