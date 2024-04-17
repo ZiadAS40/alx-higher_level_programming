87e13e230e9dadbe84fc3cb32013ee0ef6e05585
ZiadAS40 <zead4036@gmail.com>
2024-04-17 19:25:18 +0200

    debbug the task number 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -11,9 +11,14 @@ file and print it
 import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-myList = []
+filename = "add_item.json"
+
+try:
+    myList = load_from_json_file(filename)
+except FileNotFoundError:
+    myList = []
+
 for arg in sys.argv[1:]:
     myList.append(arg)
-filename = "add_item.json"
+
 save_to_json_file(myList, filename)
-data = load_from_json_file(filename)
