61b07dfc4297320ee348e0b89b6302d1a829dba1
ZiadAS40 <zead4036@gmail.com>
2024-04-17 19:16:45 +0200

    fix some bugs

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -5,5 +5,5 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """it serialize an object and save it on a file(filename)"""
-    with open(filename, "r", encoding="utf-8") as file:
+    with open(filename, "w", encoding="utf-8") as file:
         json.dump(my_obj, file)
