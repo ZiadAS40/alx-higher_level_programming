a00ef4c56d863c4280c0af0b5b9253d485f6dfbe
ZiadAS40 <zead4036@gmail.com>
2024-04-18 16:45:52 +0200

    adding the tech interview perp task

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python3
-def read_file(filename=""):
-    with open(filename) as file:
-        result = file.read()
-        print(result)
+def pascal_triangle(n):
+    if n > 1:
+        t_list = [1]
+    else:
+        return []
+    result_list = []
+    for n in range(n):
+        result_list.append(t_list)
+        t_list.append(0)
+        t_list.insert(0, 0)
+        eTempList = [t_list[i] + t_list[i + 1] for i in range(len(t_list) - 1)]
+        t_list.pop()
+        t_list.pop(0)
+        t_list = eTempList
+    return result_list
