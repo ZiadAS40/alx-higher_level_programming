4804cf5bbe4a8b3208279618176e32ebbc3aec1b
ZiadAS40 <zead4036@gmail.com>
2024-04-18 13:47:46 +0200

    adding the task numer 9

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""Stuent class"""
+
+
+class Student():
+    """defines the __init__ and
+    to_json() functions
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """retrives a dictionary representation of the
+        Student Class
+        """
+        return self.__dict__
