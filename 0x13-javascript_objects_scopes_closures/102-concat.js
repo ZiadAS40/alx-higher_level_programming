7a96e8579af737ed6a5342d8355b5bb63aeb9dd1
ZiadAS40 <zead4036@gmail.com>
2024-06-21 02:33:44 +0300

    fix some buggs

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-const f = require('f');
-const a = f.readFileSync(process.argv[2], 'utf8', function (err, result) { if (err) console.log('error', err); });
-const b = f.readFileSync(process.argv[3], 'utf8', function (err, result) { if (err) console.log('error', err); });
+const fs = require('fs');
+const a = fs.readFileSync(process.argv[2], 'utf8', function (err, result) { if (err) console.log('error', err); });
+const b = fs.readFileSync(process.argv[3], 'utf8', function (err, result) { if (err) console.log('error', err); });
 const c = a.concat(b);
-f.writeFile(process.argv[4], c, 'utf8', function (err, result) { if (err) console.log('error', err); });
+fs.writeFile(process.argv[4], c, 'utf8', function (err, result) { if (err) console.log('error', err); });
