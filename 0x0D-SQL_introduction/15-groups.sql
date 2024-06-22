08af320666ed2619890ab8ca54f7e62faa10b3c8
ZiadAS40 <zead4036@gmail.com>
2024-06-22 14:00:24 +0300

    final commit

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,4 +1,5 @@
 -- show the number of appearance of a record
-SELECT score, COUNT(*) AS number
-FROM second_table
-GROUP BY score;
+SELECT `score`, COUNT(*) AS number
+FROM `second_table`
+GROUP BY `score`
+ORDER BY `number` DESC;
