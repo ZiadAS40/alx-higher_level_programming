150ffcf9d26596f5e9f33ef622a513b26d9b73e1
ZiadAS40 <zead4036@gmail.com>
2024-06-25 21:22:03 +0300

    first commit

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,5 @@
+-- creating the first table
+CREATE table IF NOT EXISTS force_name(
+    id INT PRIMARY KEY,
+    name VARCHAR(256) NOT NULL
+);
---
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,5 @@
+-- creates another table
+CREATE table IF NOT EXISTS id_not_null (
+    id INT DEFAULT 1,
+    name VARCHAR(256)
+);
