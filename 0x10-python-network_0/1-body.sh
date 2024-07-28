974976b36c14bc8aeef8801959622794d4a8df8e
ZiadAS40 <zead4036@gmail.com>
2024-07-28 19:34:00 +0300

    make some changes

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-# sends a GET request and then diplay
-#+ the body of the response only if
-#+staus code is 200.
-response=$(curl -s -I -X GET "$1" | head -n 1 | awk '{print $2}')
-if [[ $response -eq 200 ]]; then
-        echo $(curl -s -X GET "$1")
-fi
+# sends a GET request and then diplay the body of the response only if staus code is 200.
+curl -sL "$1"
